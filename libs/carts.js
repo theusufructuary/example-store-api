7cb7fbcb7a318611d626c838a95dbfa199733f89
theusufructuary <pentamingforall@gmail.com>
2022-06-12 12:58:07 +0100

    generated mock data for API

---
+++ libs/carts.js
@@ -0,0 +1,9 @@
+const carts = [{
+    "cart_id": 1,
+    "contents": [{"course_id": "3c1a2552-ae6a-465d-aaaf-d279b50508bf"}, {"course_id": "b34294f9-a55b-436e-a89d-d56bfc462401"} ]
+  }, {
+    "cart_id": 2,
+    "contents": [{"course_id": "0e0e6930-2db0-4d80-9297-849c3cf24f2b"}, {"course_id": "ba796b71-3e1d-4298-83ee-25621afd14fe"}, {"course_id": "6dce5259-6723-4cbc-a445-e0a296b0b1bb"} ]
+  }]
+
+  export default carts;
