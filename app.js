4e710c389b3e22c3733cec51a2c3b762a3516d12
theusufructuary <pentamingforall@gmail.com>
2022-06-12 17:31:48 +0100

    attempt at adding in Heroku deployment credentials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 import coursesRouter from './routes/courses.js';
 import cartsRouter from './routes/carts.js';
-
+import { PGPORT } from './config';
 const app = express();
 
 app.use('/courses', coursesRouter);
@@ -12,6 +12,6 @@ app.get('/', (req, res) => {
     res.send(`Hello from / route`)
 });
 
-const server = app.listen(process.env.PORT, () => console.log(`Server running on port: http://localhost:${process.env.PORT}`))
+const server = app.listen(PGPORT, () => console.log(`Server running on port: http://localhost:${PGPORT}`))
 
 export default server;
