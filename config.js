efbd70ca934bb7cb644baf7c24ab7d9fa02664dd
theusufructuary <pentamingforall@gmail.com>
2022-06-29 14:27:48 +0100

    changing from several env vars to the Heroku database url connection string

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-export const PGUSER = process.env.PGUSER;
-export const PGHOST = process.env.PGHOST;
-export const PGDATABASE = process.env.PGDATABASE;
-export const PGPASSWORD = process.env.PGPASSWORD;
-export const PGPORT = process.env.PGPORT;
+export const connectionString = process.env.DATABASE_URL;
