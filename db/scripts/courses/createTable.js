93d2284dfe5a9908f8fa80693c8d123e01828961
theusufructuary <pentamingforall@gmail.com>
2022-06-29 15:17:54 +0100

    Inserting console log for testing in dev environment

--- db/scripts/courses/createTable.js
+++ db/scripts/courses/createTable.js
@@ -3,3 +3,5 @@ import query from "../../connection.js";
 const response = await query(
   `CREATE TABLE IF NOT EXISTS courses (course_id SERIAL PRIMARY KEY, title TEXT, subtitle TEXT, subscribe TEXT, price TEXT, options TEXT);`
 );
+
+console.log(response);
