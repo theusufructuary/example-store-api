48a39c6386a5488be54b25a3ce106bf652d5e04a
theusufructuary <pentamingforall@gmail.com>
2022-06-29 14:38:23 +0100

    Created Postgres query for creating table

---
+++ db/scripts/courses/createTable.js
@@ -0,0 +1,5 @@
+import query from "../../connection.js";
+
+const response = await query(
+  `CREATE TABLE IF NOT EXISTS courses (course_id SERIAL PRIMARY KEY, title TEXT, subtitle TEXT, subscribe TEXT, price TEXT, options TEXT);`
+);
